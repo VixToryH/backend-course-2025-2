7d89daef3ff5266f6727771ac7cf892da4416b63
VixToryH bc2025-2 <Viktoriia.Hvozdevych@lnu.edu.ua>
2025-09-25 19:37:21 +0300

    add Part III: main.js program and response_part3.json

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+// Константи
+const currency = "EUR";
+const exact_date = "10.09.2024";
 
+// Створення базового URL
+const myUrl = new URL("/NBU_ovdp", "https://bank.gov.ua/");
+
+// Параметри
+myUrl.searchParams.append("valcode", currency);
+myUrl.searchParams.append("date", exact_date);
+myUrl.searchParams.append("json", "");
+
+// Вивід гототового URL у консоль
+console.log("Сконструйований URL:", myUrl.toString());
